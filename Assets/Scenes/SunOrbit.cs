b48c1a080f90a93b9e5ed6ca5593a63080f4233d
Daniel Hao <danielhao461997@gmail.com>
2018-09-06 11:29:26 +1000

    cleaning, implemented sun orbit

--- Assets/Scenes/SunOrbit.cs
+++ Assets/Scenes/SunOrbit.cs
@@ -2,47 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-// Note: Code is taken from the CubeOrbit.cs file made and adjusted in Lab 2 where we spun the cube around
-// another target. In this instance, this orbit script is used to make the sun rotate around a given point
-// to mimic and the rise and fall of the sun over the course of a day
 public class SunOrbit : MonoBehaviour
 {
-    // declare the target which the sun will orbit around (here, a Cube gameobject with the mesh renderer
-    // turned off). Also declare the distance at which the sun will rotate around it, and its speed.
     public Transform target;
-    public float orbitDistance = 5000.0f;
-    public float orbitDegreesPerSec = 10.0f;
-
+    public Material terrain;
+    public float radius = 100.0f;
+    public float angularVelocity = 40.0f;
+    
     // Use this for initialization
-    // Nothing happens on running
     void Start()
-    {
-
+    {           
     }
 
-    // function to make the target orbit around another
+    // function taken from lab 2 to create orbit around the target
     void Orbit()
     {
-        if (target != null)
-        {
-            // Keep us at orbitDistance from target
-            // change the position so that its always a set distance from the targets position
-            transform.position = target.position + (transform.position - target.position).normalized * orbitDistance;
-
-            // public void RotateAround(Vector3 point, Vector3 axis, float angle);
-            // in this case, using Vector3.right because we want it to rotate into the mesh and below it to mimic
-            // night and day.
-            transform.RotateAround(target.position, Vector3.right, orbitDegreesPerSec * Time.deltaTime);
-        }
+        // set a constant radius of orbit
+        transform.position = target.position + (transform.position - target.position).normalized * radius;
+        // make the object rotate around the target
+        transform.RotateAround(target.position, Vector3.right, angularVelocity * Time.deltaTime);
+        
     }
 
-    // Call from LateUpdate if you want to be sure your
-    // target is done with it's move.
-    void LateUpdate()
+    void Update()
     {
-
         Orbit();
+        // set the point light position to be the same as the sun to create the lighting effect of sun rise and fall
+        terrain.SetVector("_PointLightPosition", this.transform.position);
 
     }
 }
